6296a6b7690c97080c9bad6d080b764ade3002ca
Han <han@alt-chain.io>
2021-11-26 16:34:19 +0800

    add refresh token

--- users/api/auth.py
+++ users/api/auth.py
@@ -1,8 +1,11 @@
 import logging
 from django.contrib.auth import authenticate, login, logout
 from django.http import JsonResponse
+from rest_framework_jwt.settings import api_settings
+from rest_framework_jwt.utils import jwt_decode_handler
 from utils import baseview
 from users.models import User
+from users.serializers import UserSerializers
 
 logger = logging.getLogger("ttool.app")
 
@@ -16,19 +19,41 @@ class LoginView(baseview.AnyLogin):
             return JsonResponse({"code": 401, "data": {}, "msg": 'username or password required'})
         username = data["username"]
         password = data["password"]
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
         user = authenticate(request, username=username, password=password)
-        if user is not None:
+        if user is not None and user.is_active:
+            payload = jwt_payload_handler(user)
+            token = jwt_encode_handler(payload)
             login(request, user)
             resp = JsonResponse(
                 {"code": 200,
-                 "data": {"username": user.get_username(), "id": user.id},
+                 "data": {"username": user.get_username(), "token": token},
                  "msg": "login success."
-                })
+                 })
             return resp
-        return JsonResponse({"code": 401, "data": {}, "msg": 'Unauthorized'})
+        return JsonResponse({"code": 404, "data": {}, "msg": 'Unauthorized'})
 
 
-class LogoutView(baseview.AnyLogin):
+class UserInfoView(baseview.BaseView):
+    "user info view"
+
+    def post(self, request, args=None):
+        token = ""
+        authorization = request.META.get('HTTP_AUTH_KEY')
+        if authorization is not None:
+            token = authorization.split(' ')[1]
+        if token == "":
+            return JsonResponse({"code": 404, "data": {}, "msg": 'need auth-key.'})
+        user_dict = jwt_decode_handler(token=token)
+        user_obj = User.objects.filter(username=user_dict["username"])
+        if user_obj.exists():
+            serializers = UserSerializers(user_obj, many=True)
+            return JsonResponse({"code": 200, "data": {"user_info": serializers.data}, "msg": "get user info"})
+        return JsonResponse({"code": 404, "data": {}, "msg": 'user not exist.'})
+
+
+class LogoutView(baseview.BaseView):
     "logout view"
 
     def post(self, request, args=None):
@@ -42,11 +67,23 @@ class RegisterView(baseview.AnyLogin):
     def post(self, request, args=None):
         data = request.data
         if "username" not in data or "password" not in data:
-            return JsonResponse({"code": 401, "data": {}, "msg": 'username or password required'})
+            return JsonResponse({"code": 404, "data": {}, "msg": 'username or password required'})
         username = data["username"]
         password = data["password"]
-        try:
-            User.objects.create_user(username=username, password=password)
-        except: # pylint: disable=bare-except
-            return JsonResponse({"code": 401, "data": {}, "msg": 'username already exist.'})
+        user = User.objects.filter(username=username)
+        if user.exists():
+            return JsonResponse({"code": 404, "data": {}, "msg": 'username already exist.'})
+        User.objects.create_user(username=username, password=password)
         return JsonResponse({"code": 200, "data": {}, "msg": 'register success.'})
+
+
+class RefreshTokenView(baseview.BaseView):
+    """refresh token view"""
+
+    def post(self, request, args=None):
+        print("refresh token：", request.user)
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+        payload = jwt_payload_handler(request.user)
+        token = jwt_encode_handler(payload)
+        return JsonResponse({"code": 200, "data": {'token': token}, "msg": "refresh token success"})
--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,9 @@ from .views import test
 
 urlpatterns = [
     path('v1/register', auth.RegisterView.as_view(), name='register'),
+    path('v1/userinfo', auth.UserInfoView.as_view(), name='userinfo'),
+    path('v1/refresh', auth.RefreshTokenView.as_view(), name='refresh'),
     path('v1/login', auth.LoginView.as_view(), name='login'),
     path('v1/logout', auth.LogoutView.as_view(), name='logout'),
-    path('v1/test', test, name='logout'),
+    path('v1/test', test, name='test'),
 ]
